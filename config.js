e54932946cd62692c460517726e3e985f0edfde5
Mayan <delhi.mayansharma@gmail.com>
2021-09-17 23:12:13 +0530

    refactor: added head tag for blogs page (SEO)

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PROD ? publicRuntimeConfig.API_PROD : publicRuntimeConfig.API_DEV;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const DOMAIN = publicRuntimeConfig.PROD ? String(publicRuntimeConfig.DOMAIN_PROD) : String(publicRuntimeConfig.DOMAIN_DEV); 
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,12 +1,34 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useState } from 'react';
+import { withRouter } from 'next/router';
 
 import Layout from '../../components/Layout';
 import { getAllBlogsWithCategoriesAndTags } from '../../actions/blog';
 import Card from '../../components/blog/Card';
+import { DOMAIN } from '../../config';
 
-const BlogPage = ({ blogs, categories, tags, size }) => {
+const BlogPage = ({ blogs, categories, tags, size, router }) => {
+
+    const head = () => (
+        <Head>
+            <title>Tech Blogs | one</title>
+            <meta name='description' content='Tech Blogs' />
+            
+            <link rel='canonical' href={`${DOMAIN}${router.pathname}`} />
+            <meta propery='og:title' content={'Latest web development tutorials | one'} />
+            <meta property='og:description' content='Tech blogs'/>
+            
+            <meta property='og:type' content='website' />
+            <meta property='og:url' content={`${DOMAIN}${router.pathname}`} />
+            <meta property='og:site_name' content='one' />
+            
+            <meta property='og:image' content={`${DOMAIN}/public/static/images/blog-image.png`} />
+            <meta property='og:image:secure_url' content={`${DOMAIN}/public/static/images/blog-image.png`} />
+            <meta property='og:image:type' content='image/png' />
+        </Head> 
+    )
+
+    console.log(router);
 
     const showAllBlogs = () => (
         blogs.map(blog => (
@@ -38,26 +60,29 @@ const BlogPage = ({ blogs, categories, tags, size }) => {
     )
 
     return (
-        <Layout>
-            <main>
-                <div className='container-fluid'>
-                    <header>
-                        <div className='col-md-12 pt-3'>
-                            <h1 className='display-4 font-weight-bold text-center'>Tech Blogs</h1>
-                        </div>
-                        <section className='text-center'>
-                            {showAllCategories()}
-                            {showAllTags()}
-                        </section>
-                    </header>
-                </div>
-                <div className='container-fluid'>
-                    <div className='row'>
-                        <div className='col-md-12'>{showAllBlogs()}</div>
+        <>
+            {head()}
+            <Layout>
+                <main>
+                    <div className='container-fluid'>
+                        <header>
+                            <div className='col-md-12 pt-3'>
+                                <h1 className='display-4 font-weight-bold text-center'>Tech Blogs</h1>
+                            </div>
+                            <section className='text-center'>
+                                {showAllCategories()}
+                                {showAllTags()}
+                            </section>
+                        </header>
                     </div>
-                </div>
-            </main>
-        </Layout>
+                    <div className='container-fluid'>
+                        <div className='row'>
+                            <div className='col-md-12'>{showAllBlogs()}</div>
+                        </div>
+                    </div>
+                </main>
+            </Layout>
+        </>
     );
 }
 
@@ -76,4 +101,4 @@ BlogPage.getInitialProps = async () => {
     }
 }
 
-export default BlogPage;
+export default withRouter(BlogPage);
