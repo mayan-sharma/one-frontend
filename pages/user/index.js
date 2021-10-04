8f3ddaacde1a99ed0c59d9f9cd584a68fb57446e
Mayan <delhi.mayansharma@gmail.com>
2021-10-04 20:00:34 +0530

    feat: added user page dashboard

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import Layout from '../../components/Layout';
 import Private from '../../components/auth/Private';
 
@@ -5,7 +7,33 @@ const UserIndexPage = () => {
     return (
         <Layout>
             <Private>
-                <p>User Dashboard</p>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col-md-12 pt-5 pb-5'>
+                            <h2>User Dashboard</h2>
+                        </div>
+                        <div className='col-md-4'>
+                            <ul className='list-group'>
+                                <li className='list-group-item'>
+                                    <Link href='/admin/crud/blog'>
+                                        <a>Create Blog</a>
+                                    </Link>
+                                </li>
+                                <li className='list-group-item'>
+                                    <Link href='/admin/crud/blogs'>
+                                        <a>Update/Delete Blogs</a>
+                                    </Link>
+                                </li>
+                                <li className='list-group-item'>
+                                    <Link href='/user/update'>
+                                        <a>Update profile</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='col-md-8'>right</div>
+                    </div>
+                </div>
             </Private>
         </Layout>
     );
