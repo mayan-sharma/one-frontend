131d300fc126094ac7ab84cfb776ace610578131
Mayan <delhi.mayansharma@gmail.com>
2021-10-06 20:04:08 +0530

    feat: added create blog page for user

---
+++ pages/user/crud/blog.js
@@ -0,0 +1,24 @@
+import Private from '../../../components/auth/Private';
+import Layout from '../../../components/Layout';
+import BlogCreate from '../../../components/crud/BlogCreate';
+
+const BlogPage = () => {
+    return (
+        <Layout>
+            <Private>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col-md-12 pt-5 pb-5'>
+                            <h2>Create a new blog</h2>
+                        </div>
+                        <div className='col-md-12'>
+                            <BlogCreate />
+                        </div>
+                    </div>
+                </div>
+            </Private>
+        </Layout>
+    );
+}
+
+export default BlogPage;
