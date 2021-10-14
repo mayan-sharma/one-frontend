7e63039c6c76277c509748e6ab3adf2e00d31ff9
Mayan <delhi.mayansharma@gmail.com>
2021-10-14 18:01:55 +0530

    refactor: changes to index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Link from 'next/link';
+
 import Layout from '../components/Layout';
 
 const IndexPage = () => {
     return (
         <Layout>
-            <p>Hello</p>
+            <div className='container text-center'>
+                <h1>Welcome to One</h1>
+                <h3>A place to share ideas.</h3>
+                <Link href='/blogs'>
+                    <a className='btn btn-primary mt-2'>Explore</a>
+                </Link>
+            </div>
         </Layout>
     );
 }
